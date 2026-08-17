a7abc62c597b99edf79ff76d5af755610c8acde9
a <102840087+kingdudely@users.noreply.github.com>
2026-08-17 16:15:39 -0400

    examples

---
+++ examples/create.js
@@ -0,0 +1,15 @@
+import Tunnel from "firetunnel";
+
+const tunnel = new Tunnel({
+	"url": "localhost:8080",
+    "metrics": "localhost:8081"
+});
+
+while (!await tunnel.isReady()) await setTimeout(1000);
+
+const { hostname } = await tunnel.getQuickTunnelInfo();
+
+!async function() {
+	const exitedInfo = await tunnel.exited;
+	
+}()
